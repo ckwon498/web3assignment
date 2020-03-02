7017bbf219a893daaff95b2b24ced07ba44147ed
Chris <ckwon498@mtroyal.ca>
2020-03-02 14:38:10 -0700

    Updates

---
+++ precache-manifest.450d21842290af403af40bf4d6ef5ffa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca234721ea91da2c81509b36bbad6c5c",
+    "url": "/web3assignment/index.html"
+  },
+  {
+    "revision": "b2a3a65c760c57d2bfda",
+    "url": "/web3assignment/static/css/main.ca7f3d87.chunk.css"
+  },
+  {
+    "revision": "fbed478a4ef98a1d4f15",
+    "url": "/web3assignment/static/js/2.f5cb749f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/web3assignment/static/js/2.f5cb749f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a3a65c760c57d2bfda",
+    "url": "/web3assignment/static/js/main.9d11fc1d.chunk.js"
+  },
+  {
+    "revision": "72f5b3904b693805d007",
+    "url": "/web3assignment/static/js/runtime-main.b3cd2ebd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web3assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
