8122effdb7e3f13c48abc0d8bb21fe4f63b8f578
Chris <ckwon498@mtroyal.ca>
2020-03-12 10:21:29 -0600

    remove dir

--- src/components/SearchFilter.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-/*this.props.filterYear;
-  this.props.filterRating;*/
-class SearchFilter extends React.Component {
-  match = this.props.movies.filter(m => m.title === this.props.titleInp);
-  title = this.props.titleInp;
-}
-
-export default SearchFilter;
